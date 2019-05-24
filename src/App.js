9eddcfd8cc42edbb3cd986f508636e1512f740c6
ydying <1778679036@qq.com>
2019-05-24 16:41:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MxGraphEditor from "./MxGraphGridEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <MxGraphEditor />
+    </div>
+  );
+}
+
+export default App;
